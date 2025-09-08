c1f7d3411f2c9747bb1a51d88282b396550f051a
Wassim-RF <wassimrifi0313@gmail.com>
2025-09-08 12:33:17 +0200

    #3

---
+++ chall3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num , i;
+    int sum = 0;
+    printf("Entrer le nombre des nombre qui tu veux : ");
+    scanf("%d" , &num);
+    int numbers[num];
+    for (i = 0 ; i < num ; i++) {
+        printf("Enter a number : ");
+        scanf("%d" , &numbers[i]);
+    }
+    printf("Votre nombre sont : ");
+    for (i = 0 ; i < num ; i++) {
+        printf("%d " , numbers[i]);
+        sum += numbers[i];
+    }
+    printf("\nLa somme de votre nombre est : %d.\n" , sum);
+    return 0;
+}
