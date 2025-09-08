a4503ea84133caf97bf9259fdd884df720956b95
Wassim-RF <wassimrifi0313@gmail.com>
2025-09-08 12:24:43 +0200

    #1

--- chall1.c
+++ chall1.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 
-int main() {}
+int main() {
+    int num[5] , i;
+    for (i = 0 ; i < 5 ; i++) {
+        printf("Enter a number : ");
+        scanf("%d" , &num[i]);
+    }
+    printf("Votre numero sont : ");
+    for (i = 0 ; i < 5 ; i++) {
+        printf("%d " , num[i]);
+    }
+    return 0;
+}
